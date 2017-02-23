defa8fe39b37df98e8948c326b256c0e567f1e90
Mrlott <nexriz@hotmail.com>
2017-02-23 13:09:36 +0100

    just init the server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-index.js
+import express from 'express';
+import ch from 'chalk';
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('heej')
+})
+
+app.listen(3000, () => {
+	console.log(ch.blue('Server is running on port ') + ch.white('3000'))
+})
